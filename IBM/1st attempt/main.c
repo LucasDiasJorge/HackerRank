bde42ef577c9eddbeb55437aa40c8dfef3b9b733
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-02-01 23:01:12 -0300

    Create main.c

---
+++ IBM/1st attempt/main.c
@@ -0,0 +1,160 @@
+//https://www.hackerrank.com/test/c8d7gn3k2b/questions/hd5r0hl8
+
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+int balancedSum(int arr_count, int* arr) {
+    int total_sum = 0;
+    int left_sum = 0;
+    
+    for(int i = 0; i < arr_count; i++){
+        total_sum += arr[i];
+    }
+    
+    for(int i = 0; i < arr_count; i++){
+        total_sum -=arr[i];
+        
+        if(left_sum == total_sum){
+            return i;
+        }
+        
+        left_sum += arr[i];
+    }
+    return -1;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int arr_count = parse_int(ltrim(rtrim(readline())));
+
+    int* arr = malloc(arr_count * sizeof(int));
+
+    for (int i = 0; i < arr_count; i++) {
+        int arr_item = parse_int(ltrim(rtrim(readline())));
+
+        *(arr + i) = arr_item;
+    }
+
+    int result = balancedSum(arr_count, arr);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
